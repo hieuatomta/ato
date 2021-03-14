d670f4c3e23d05bfed05ac6445cc06dc33d3cb9b
thachhieu <hieuatoatomta4@gmail.com>
2021-03-14 23:39:07 +0700

    hieutt - update thêm phần thanh toán  qua zaloPay

--- src/app/client/shoping-cart/shoping-cart.component.ts
+++ src/app/client/shoping-cart/shoping-cart.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import {OrderService} from '../../@core/services/order.service';
 
 
 @Component({
@@ -11,7 +12,11 @@ import {FormControl, FormGroup} from '@angular/forms';
 export class ShopingCartComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
   }
+
   inputForm: any;
+
+  constructor(private orderService: OrderService) {
+  }
 
   soluong(e) {
     let soLuong = this.inputForm.get('amount').value;
@@ -27,8 +32,10 @@ export class ShopingCartComponent implements OnInit, OnDestroy {
     this.inputForm.get('amount').setValue(soLuong);
     return soLuong;
   }
+
   obj = null;
   size = null;
+
   ngOnInit(): void {
     this.obj = JSON.parse(localStorage.getItem('list_order'));
     if (this.obj === null || this.obj === undefined) {
@@ -44,5 +51,19 @@ export class ShopingCartComponent implements OnInit, OnDestroy {
       cost: new FormControl(null, []),
       name: new FormControl(null, [])
     });
+  }
+
+  thanhToan() {
+    this.orderService.insert(null).subscribe(res => {
+        if (res.body.data.returncode === 1) {
+          console.log(res.body.data.orderurl);
+          window.location = res.body.data.orderurl;
+        } else {
+          console.log('That bai');
+        }
+      }, (error) => {
+        // this.isLoad = false;
+      },
+    );
   }
 }
